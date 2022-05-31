3fc7d01d5c32d85d8d840fbbf66926452a89ca4e
devsergiosm <sergio.huanacchiri@gmail.com>
2022-05-31 18:58:50 -0500

    Version final

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const predictBtn = document.querySelector('#btnPredict');
 const resultContainer = document.querySelector('#resultContainer');
+const clearButton = document.getElementById('clear-button');
 
 cargarEventListeners();
 function cargarEventListeners(){
@@ -7,77 +8,83 @@ function cargarEventListeners(){
 }
 
 function predict(){
-    let safety = document.querySelector('.form .safety select').value;
-    let persons = document.querySelector('.form .persons input').value;
-    let buying = document.querySelector('.form .buying select').value;
-    let doors = document.querySelector('.form .doors input').value;
-    let valuePredicted = document.querySelector('#result')
-
     try{
-        persons = parseInt(persons)
-        doors = parseInt(doors)
+        let persons = document.querySelector('input[name=persons]:checked').value;
+        let doors = document.querySelector('input[name=persons]:checked').value;
 
-        if (isNaN(persons) || isNaN(doors)) throw "Se esperaba dato numérico";
-        if (safety=="none" || doors=="none" ) throw "Datos incompletos";
+        let safety = document.querySelector('.form .safety select').value;
+        let buying = document.querySelector('.form .buying select').value;
+        let valuePredicted = document.querySelector('#result')
+    
+        try{
+            persons = parseInt(persons)
+            doors = parseInt(doors)
+    
+            if (isNaN(persons) || isNaN(doors)) throw "Se esperaba dato numérico";
+            if (safety=="none" || doors=="none" || persons == null || doors == null ) throw "Datos incompletos";
+        }catch(e){
+            alert(e)
+        }
+    
+        console.log(safety,persons,buying,doors);
+    
+        result = calc(safety,persons,buying,doors);
+    
+        console.log(result);
+        valuePredicted.textContent = result;
+
     }catch(e){
-        alert(e)
+        alert("Datos incompletos")
     }
-
-    console.log(safety,persons,buying,doors);
-
-    result = calc(safety,persons,buying,doors);
-
-    console.log(result);
-    valuePredicted.textContent = result;
 }
 
 function calc(safety,persons,buying,doors){
     let result='';
     if(safety=='low'){
-        result='unacc';
+        result='NO';
     }else if(safety==='med'){
         if(persons<=3){
-            result='unacc';
+            result='NO';
         }else if(persons>3){
             if(buying==='vhigh'){
-                result='unacc';
+                result='NO';
             }else if(buying==='high'){
-                result='unacc';
+                result='NO';
             }else if(buying==='med'){
                 if(doors<=2.5){
                     if(persons<=4.5){
-                        result='acc';
+                        result='yes';
                     }else if(persons>4.5){
-                        result='unacc';
+                        result='NO';
                     }
                 }else if(doors>2.5){
-                    result='acc';
+                    result='yes';
                 }
             }else if(buying==='low'){
-                result='acc';
+                result='yes';
             }
         }
     }else if(safety==='high'){
         if(persons<=3){
-            result='unacc';
+            result='NO';
         }else if(persons>3){
             if(buying==='vhigh'){
-                result='unacc';
+                result='NO';
             }else if(buying==='high'){
-                result='acc';
+                result='yes';
             }else if(buying==='med'){
-                result='acc';
+                result='yes';
             }else if(buying==='low'){
                 if(doors<=2.5){
                     if(persons<=4.5){
-                        result='acc';
+                        result='yes';
                     }else if(persons>4.5){
-                        result='unacc';
+                        result='NO';
                     }
                 }else if(doors>2.5){
                    if(doors<=3.5){
                         if(persons<=4.5){
-                            result='acc'
+                            result='yes'
                         }else if(persons>4.5){
                             result='vgood';
                         }
